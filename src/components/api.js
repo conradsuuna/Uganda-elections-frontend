324636c4b1c4a7b9cd0246fdd1a1c952cf9d57cb
conradsuuna <conradsuuna.cs@gmail.com>
2022-01-17 00:05:05 +0300

    edit base url

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const token = 0
 export default axios.create({
-	baseURL: `http://127.0.0.1:5000/`,
+	baseURL: `https://uganda-elections-analysis.herokuapp.com/`,
 	headers: {
 		Authorization: `Bearer ` +token
 	}
