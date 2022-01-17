19e4b66ab00b5139d1c60b950ba3f63d8386a0f0
conradsuuna <conradsuuna.cs@gmail.com>
2022-01-17 11:48:52 +0300

    hook modification

--- src/components/charts/CustomHook.js
+++ src/components/charts/CustomHook.js
@@ -1,12 +1,12 @@
 import React from "react";
 import * as d3 from 'd3';
 
-export const useD3 = (renderChartFn, dependencies) => {
+export const useD3 = (renderChartFn) => {
     const ref = React.useRef();
 
     React.useEffect(() => {
         renderChartFn(d3.select(ref.current));
         return () => { };
-    }, dependencies);
+    });
     return ref;
 }
